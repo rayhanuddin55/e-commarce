035989bcce9336a9250cc620dcd26d20a1756ca1
Rayhan Uddin <rayhanuddin55@gmail.com>
2016-05-24 16:56:37 +0600

    final submit

---
+++ UltraShopBD.WebUI/BInders/CartModelBinder.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UltraShopBd.Domain.Entities;
+
+namespace UltraShopBd.WebUI.BInders
+{
+    public class CartModelBinder : IModelBinder
+    {
+        private const string sessionkey = "Cart";
+    
+        public object BindModel(ControllerContext controllerContext, ModelBindingContext bindingContext)
+        {
+            Cart cart = null;
+            if(controllerContext.HttpContext.Session !=null)
+            {
+                cart = (Cart)controllerContext.HttpContext.Session[sessionkey];
+
+            }
+            if(cart== null)
+            {
+                cart = new Cart();
+                if (controllerContext.HttpContext.Session != null)
+                    controllerContext.HttpContext.Session[sessionkey] = cart;
+            }
+            return cart;
+        }
+}
+}
